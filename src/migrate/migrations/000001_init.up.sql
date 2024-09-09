52ec126da9ce2f44c40a215ea4681ba7971b4aed
Eldar Bagirov <eldar@bagirov.ru>
2024-09-09 21:55:04 +0300

    fix: db uuid data type

--- src/migrate/migrations/000001_init.up.sql
+++ src/migrate/migrations/000001_init.up.sql
@@ -26,35 +26,35 @@ CREATE TYPE bid_author_type AS ENUM (
 );
 
 CREATE TABLE tender (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR(100) NOT NULL,
     description TEXT,
     service_type tender_service_type,
     status tender_status,
-    organization_id INT REFERENCES organization(id) ON DELETE CASCADE,
+    organization_id UUID REFERENCES organization(id) ON DELETE CASCADE,
     version INT DEFAULT 1 CHECK (version > 0),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE bid (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR(100) NOT NULL,
     description TEXT,
     status bid_status,
-    tender_id INT REFERENCES tender(id) ON DELETE CASCADE,
+    tender_id UUID REFERENCES tender(id) ON DELETE CASCADE,
     author_type bid_author_type,
-    author_id INT REFERENCES employee(id) ON DELETE SET NULL,
+    author_id UUID REFERENCES employee(id) ON DELETE SET NULL,
     version INT DEFAULT 1 CHECK (version > 0),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE bid_review (
-    id SERIAL PRIMARY KEY,
-    bid_id INT REFERENCES bid(id) ON DELETE CASCADE,
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    bid_id UUID REFERENCES bid(id) ON DELETE CASCADE,
     review_description TEXT,
-    reviewed_by INT REFERENCES employee(id) ON DELETE SET NULL,
+    reviewed_by UUID REFERENCES employee(id) ON DELETE SET NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
